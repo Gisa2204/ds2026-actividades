11eae1dcc38de551d2dd1d030988301b618ea39a
Gino <ginosalvagnini12@gmail.com>
2026-05-23 22:40:13 -0300

    Librería con React-TypeScript + Bootstrap

---
+++ ejercicios/c08-libreria-react/libreria-react/src/components/CardLibro.tsx
@@ -0,0 +1,39 @@
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+
+interface cardLibroProps {
+  titulo: string;
+  autor: string;
+  precio: number,
+  imgSrc: string;
+}
+
+function CardLibro({ titulo, autor, precio, imgSrc }: cardLibroProps) {
+  
+  const [itemCarrito, setItemCarrito] = useState(0);
+  const incrementarCarrito = () => {
+    setItemCarrito(itemCarrito + 1);
+  }
+  
+  return (
+    <Col lg={4}>
+      <Card className='px-1 py-1'>
+        <Card.Img variant="top" src={imgSrc} height={600} />
+        <Card.Body>
+          <Card.Title>{titulo}</Card.Title>
+          <Card.Subtitle className='py-1'>{autor}</Card.Subtitle>
+          <Card.Subtitle className='py-3 fs-5' style={{color: "green"}}>${precio}</Card.Subtitle>
+        </Card.Body>
+        <Button 
+          variant="outline-primary" size='lg'
+          onClick={ incrementarCarrito }
+          >Agregar al carrito ({itemCarrito})
+        </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default CardLibro;
---
+++ ejercicios/c08-libreria-react/libreria-react/src/components/Hero.tsx
@@ -0,0 +1,19 @@
+function Hero() {
+    return (
+        <>
+            <section className="hero-bg-img text-white py-5 shadow-lg">
+                <div className="container py-5">
+                    <div className="row">
+                        <div className="col-lg-7">
+                            <h1 className="display-2 fw-bold mb-3">Explora los confines del universo.</h1>
+                            <p className="lead mb-4 fs-2">Explora miles de títulos en nuestra colección exclusiva.</p>
+                            <a className="btn btn-outline-light fs-4">Comenzar lectura</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Hero;
